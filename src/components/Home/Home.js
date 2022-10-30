25a722202e361d9380329687e65778a201b13a4e
Jae <giffordjacob0@gmail.com>
2022-10-30 13:27:48 +0800

    button and color animate on hover

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import bomberURL from '../../assets/products/bomber2k.png';
 import '../../assets/fonts/futuremoon.ttf';
 import githubPinkIcon from '../../assets/icons/github-pink.png';
 import instaPinkIcon from '../../assets/icons/insta-pink.png';
-
 import styles from './home.module.css';
 
 export default function Home() {
+  const [isCircleHovered, setIsCircleHovered] = useState(false);
+
+  const onMouseInHandler = () => {
+    setIsCircleHovered(true);
+  };
+  const onMouseOutHandler = () => {
+    setIsCircleHovered(false);
+  };
+
   return (
     <div className={styles.homeContainer}>
       <div>
-        <div>
-          <h2 className={styles.productNameFont}>Breezy Bomber</h2>
-          <p>
-            Out main product line this year is inspired heavily by CyberPunk.
-            The theme draws heavily from Tokyo Night and grungy street styles to
-            create an effortlessly cool and complex blend of old and new.
-          </p>
-          <button>Tokyo Night Line</button>
+        <div className={styles.padding}>
+          <div className={styles.innerLeft}>
+            <h2 className={styles.productNameFont}>Breezy Bomber</h2>
+            <p className={styles.homeDescriptionP}>
+              Out main product line this year is inspired heavily by CyberPunk.
+              The theme draws from Tokyo Night and grungy street styles to
+              create an effortlessly cool and complex blend of old and new.
+            </p>
+            <br></br>
+            <p className={styles.homeSubDescription}>
+              Sourced ethically from Japan, our fabrics seamlessly blend a
+              luxurious cotton feel with a sleek matte finish.
+            </p>
+          </div>
+          <button>
+            <div className='original'>Tokyo Night Line</div>
+            <div className='letters'>
+              <span>Tok</span>
+              <span>yo</span>
+              <span>Nig</span>
+              <span>ht</span>
+              <span>Li</span>
+              <span>ne</span>
+            </div>
+          </button>
         </div>
-        <div>
-          <img
-            className={styles.socialsIcons}
-            src={githubPinkIcon}
-            alt='github'
-          ></img>
+        <div className={styles.socialsWrapper}>
+          <a href='https://github.com/JaeGif' rel='noreferrer' target='_blank'>
+            <img
+              className={styles.socialsIcons}
+              src={githubPinkIcon}
+              alt='github'
+            ></img>
+          </a>
           <img
             className={styles.socialsIcons}
             src={instaPinkIcon}
@@ -32,13 +59,21 @@ export default function Home() {
           ></img>
         </div>
       </div>
-      <div className={styles.imgContainer}>
+      <div
+        className={styles.imgContainer}
+        onMouseEnter={onMouseInHandler}
+        onMouseLeave={onMouseOutHandler}
+      >
         <img
           className={styles.mainProductImg}
           src={bomberURL}
           alt='main-spread-img'
         />
-        <div className={styles.circleFlair}></div>
+        <div
+          className={
+            isCircleHovered ? styles.circleFlairHover : styles.circleFlair
+          }
+        ></div>
       </div>
     </div>
   );
